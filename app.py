3969e01a2705a0c38c401ea4bb2cda8c0cfc5683
Cesar <cesartellez301@gmail.com>
2025-02-24 11:16:35 -0600

    crudo completo

--- app.py
+++ app.py
@@ -30,14 +30,13 @@ class Alumno(db.Model):
 #Ruta raiz
 @app.route('/')
 def index():
-    #retornar alumnos
     alumnos = Alumno.query.all()
     return render_template('index.html', alumnos = alumnos)
 
+#Crear alumno
 @app.route('/alumnos/new', methods=['GET','POST'])
 def create_alumno():
     if request.method == 'POST':
-        #Agregar Alumno
         no_control = request.form['no_control']
         nombre = request.form['nombre']
         ap_paterno = request.form['ap_paterno']
@@ -50,9 +49,29 @@ def create_alumno():
         db.session.commit()
     
         return redirect(url_for('index'))
-    
-    
     return render_template('create_alumno.html')
 
+#Eliminar alumno
+@app.route('/alumnos/delete/<string:no_control>')
+def delete_alumno(no_control):
+    alumno = Alumno.query.get(no_control)
+    if alumno:
+        db.session.delete(alumno)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+#Actualizar alumno
+@app.route('/alumnos/update/<string:no_control>', methods=['GET','POST'])
+def update_alumno(no_control):
+    alumno = Alumno.query.get(no_control)
+    if request.method == 'POST':
+        alumno.nombre = request.form['nombre']
+        alumno.ap_paterno = request.form['ap_paterno']
+        alumno.ap_materno = request.form['ap_materno']
+        alumno.semestre = request.form['semestre']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update_alumno.html', alumno=alumno)
+    
 if __name__ == '__main__':
     app.run(debug=True)
